62a32e22284dd358b6e57747fe695c0f3031474a
quirkexplorer <quirkyexplorer@gmail.com>
2024-06-29 18:24:47 -0700

    'added delete api '

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -20,3 +20,65 @@ export async function POST(request: NextRequest) {
         return NextResponse.json({error: error}.error, {status: 400});
       }
 }
+
+/// TODO FIX 
+export async function PUT(request: NextRequest) {
+  let result;
+  try {
+    const data = await request.json();
+
+    console.log('request', data)
+    
+    const result = await prisma.product.create({
+      data
+    });
+
+    return NextResponse.json(JSON.stringify(result), { status: 201 });
+  } catch(error) {
+    console.log('Error processing request: ', error);
+
+    return NextResponse.json({error: error}.error, {status: 400});
+  }
+}
+
+
+// TODO FIX 
+export async function GET(request: NextRequest) {
+  let result;
+  try {
+    const data = await request.json();
+
+    console.log('request', data)
+    
+    const result = await prisma.product.create({
+      data
+    });
+
+    return NextResponse.json(JSON.stringify(result), { status: 201 });
+  } catch(error) {
+    console.log('Error processing request: ', error);
+
+    return NextResponse.json({error: error}.error, {status: 400});
+  }
+}
+
+export async function DELETE(request: NextRequest) {
+  let result;
+  try {
+    const data = await request.json();
+
+    console.log('request', data)
+    
+    const result = await prisma.product.delete({
+      where: {
+        id: data.id
+      }
+    }) 
+
+    return  new NextResponse(null, { status: 204 });
+  } catch(error) {
+    console.log('Error processing request: ', error);
+
+    return NextResponse.json({error: error}.error, {status: 400});
+  }
+}
